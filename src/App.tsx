ae66d32e5f494fe1971d924d6c6a670da41f10ae
Marleine Daoud <madaoud@microsoft.com>
2020-06-21 20:52:37 -0700

    Fixed build errors only shown by doing 'npm run build' (didn't show with 'npm start' only). Also added click handler for the Card component to log to console the card title.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import * as React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
-const cards = [{ key:1, title: 'one', content: 'one content' },
-{ key:2, title: 'two', content: 'two content' },
-{ key:3, title: 'three', content: 'three content' },
-{ key:4, title: 'four', content: 'four content' }];
+const cards = [{ key: 1, title: 'one', content: 'one content' },
+{ key: 2, title: 'two', content: 'two content' },
+{ key: 3, title: 'three', content: 'three content' },
+{ key: 4, title: 'four', content: 'four content' }];
 
 export function App() {
   const list = ['Marleine', 'Andrew'];
@@ -14,7 +14,7 @@ export function App() {
 
   const onClickChangeName = React.useCallback(() => {
     setIndex((index + 1) % list.length)
-  }, [index]);
+  }, [index, list.length]);
 
   const name = `${list[index]} ${counterValue !== undefined ? counterValue : ''}`;
   const changeValue = React.useCallback(() => {
@@ -29,7 +29,7 @@ export function App() {
       {counterValue === undefined && <AppCounter onSubmit={setCounterValue} />}
       <div className={"app-card-list"}>
         {
-          cards.map(card => <Card key={card.key} title={card.title} content={card.content}/>)
+          cards.map(card => <Card key={card.key} title={card.title} content={card.content} />)
         }
       </div>
       <div className="App">
@@ -46,12 +46,13 @@ interface AppHeaderProps {
   title?: string;
 }
 
-function AppHeader(props: AppHeaderProps) {
+export function AppHeader(props: AppHeaderProps) {
   const { title } = props;
   return <h1>Title: {title}</h1>;
 }
 
 function AppCounter(props: { onSubmit: (value: number) => void }) {
+  const { onSubmit } = props;
   const [counterValue, setCounterValue] = React.useState(0);
 
   const onClickPlus = React.useCallback(() => {
@@ -62,24 +63,28 @@ function AppCounter(props: { onSubmit: (value: number) => void }) {
     setCounterValue(counterValue - 1)
   }, [counterValue]);
 
-  const onSubmit = React.useCallback(() => {
-    props.onSubmit(counterValue);
-  }, [props.onSubmit, counterValue])
+  const onSubmitInternal = React.useCallback(() => {
+    onSubmit(counterValue);
+  }, [onSubmit, counterValue])
   return <>
     <h4>Counter value: {counterValue}</h4>
     <button onClick={onClickPlus}>+</button>
     <button onClick={onClickMinus}>-</button>
-    <button onClick={onSubmit}>Submit</button>
+    <button onClick={onSubmitInternal}>Submit</button>
   </>;
 }
 
 function Card(props: { title: string, content?: string }) {
-  return <div className={"app-card"}>
+  const { title, content } = props;
+  const logTitleToConsole = React.useCallback(() => {
+    console.log(title);
+  }, [title]);
+  return <div className={"app-card"} onClick={logTitleToConsole}>
     <div className={"app-card-title"}>
-      {props.title}
+      {title}
     </div>
     <div className={"app-card-content"}>
-      {props.content}
+      {content}
     </div>
   </div>;
 }
